77b4e9bf0d8dbf8819c4d4004fbd2ea61db5270b
jhyeokqq <130131572+jhyeokqq@users.noreply.github.com>
2024-08-02 15:27:17 +0900

    Add files via upload

---
+++ 19.Rain Alert/main.py
@@ -0,0 +1,41 @@
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = "YOUR ACCOUNT SID"
+auth_token = os.environ.get("AUTH_TOKEN")
+
+weather_params = {
+    "lat": "LATITUDE",
+    "lon": "LONGITUDE",
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="Rain today ☔️",
+        from_="FROM NUMBER",
+        to="MY NUMBER"
+    )
+    print(message.status)
