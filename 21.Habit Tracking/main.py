ee3ae77e18943adbb857093b44fad956a18c7eff
jhyeokqq <130131572+jhyeokqq@users.noreply.github.com>
2024-08-07 10:56:12 +0900

    Update main.py

--- 21.Habit Tracking/main.py
+++ 21.Habit Tracking/main.py
@@ -1,9 +1,9 @@
 import requests
 from datetime import datetime
 
-USERNAME = "USERNAME"
-TOKEN = "SELF GENERATED TOKEN"
-GRAPH_ID = "GRAPH ID"
+USERNAME = "YOUR USERNAME"
+TOKEN = "YOUR SELF GENERATED TOKEN"
+GRAPH_ID = "YOUR GRAPH ID"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -13,6 +13,10 @@ user_params = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
+
+## POST
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
@@ -28,23 +32,36 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
 pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
 today = datetime.now()
+# print(today.strftime("%Y%m%d"))
 
 pixel_data = {
     "date": today.strftime("%Y%m%d"),
-    "quantity": input("How many kilometers did you cycle today? "),
+    "quantity": input(""),
 }
 
-response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
-print(response.text)
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+# print(response.text)
 
 update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
 
 new_pixel_data = {
-    "quantity": "4.5"
+    "quantity": ""
 }
+
+## PUT
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
 
 
 delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+
+## DELETE
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
